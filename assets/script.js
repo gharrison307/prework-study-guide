aa6403c1c401b30b3b670f91ff62467ed7307301
George Harrison <g.a.harrison91@gmail.com>
2023-01-29 15:03:42 -0700

    fixed error in JS function

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,6 @@ if (randomTopic === 'HTML') {
 }
 }
 console.log('Here are the topics we learned through Prework:');
-listTopics()
+listTopics();
 console.log('Which topic should we study first?');
-selectTopic()
+selectTopic();
